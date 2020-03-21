c0c723149fa1e1b2803e8807b1246782a547760b
Thomas Heller <thomas.m.heller@gmail.com>
2020-03-21 14:23:40 +0100

    delete PushService

--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/PushService.java
+++
@@ -1,9 +0,0 @@
-package de.govhackathon.wvsvcoronatracker.services;
-
-public interface PushService {
-
-    /**
-     * Send push notiication to specific device.
-     */
-    void sendPushToOneDevice(final String appId, final string title, final string message, final string deviceToken);
-}
