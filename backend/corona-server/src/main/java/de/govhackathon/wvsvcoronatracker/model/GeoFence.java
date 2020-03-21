55943e6d901ad4769fae3f8804971702d4170ca4
masc-z <martin.schmidt@zalando.de>
2020-03-21 16:33:49 +0100

    fix data classes geofence and position

--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/GeoFence.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/GeoFence.java
@@ -1,19 +1,30 @@
 package de.govhackathon.wvsvcoronatracker.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Builder
+@Table(name = "GEOFENCE")
+@Entity
+@NoArgsConstructor
 public class GeoFence {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name ;
+    private String name;
 
-    private String description ;
+    private String description;
 
     private String area;
 }
--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/Position.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/Position.java
@@ -1,7 +1,12 @@
 package de.govhackathon.wvsvcoronatracker.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -10,8 +15,12 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Builder
+@Entity
+@NoArgsConstructor
 public class Position {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private Integer userId;
