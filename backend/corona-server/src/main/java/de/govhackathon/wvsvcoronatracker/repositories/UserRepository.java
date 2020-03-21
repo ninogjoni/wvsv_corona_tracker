2a9f71fcb4f5587ca5e6a10b6660f8c811ca9862
Helge Neven <helge.neven@hagebau.com>
2020-03-21 14:47:49 +0100

    implemented UserController, UserService and UserRepository

---
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package de.govhackathon.wvsvcoronatracker.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import de.govhackathon.wvsvcoronatracker.model.User;
+
+public interface UserRepository extends CrudRepository<User, String> {
+}
--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/UsersService.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/UsersService.java
@@ -1,39 +1,47 @@
 package de.govhackathon.wvsvcoronatracker.services;
 
-import de.govhackathon.wvsvcoronatracker.model.User;
-
 import java.util.List;
+import java.util.Optional;
+
+import de.govhackathon.wvsvcoronatracker.model.User;
 
 public interface UsersService {
 
-    /**
-     * Get all users
-     *
-     * @return List of all users, empty collection if none available
-     */
-    List<User> getUsers();
+  /**
+   * Get all users
+   *
+   * @return List of all users, empty collection if none available
+   */
+  List<User> getUsers();
 
-    /**
-     * Create a user
-     *
-     * @param user data to create user from
-     * @return Created user
-     */
-    User createUser(final User user);
+  /**
+   * Get a user by id
+   *
+   * @return users or null
+   */
+  Optional<User> getUser(String id);
 
-    /**
-     * Update a user
-     *
-     * @param id   users id
-     * @param user user data to be changed
-     * @return Updated user
-     */
-    User updateUser(final Integer id, final User user);
+  /**
+   * Create a user
+   *
+   * @param user data to create user from
+   * @return Created user
+   */
+  User createUser(final User user);
 
-    /**
-     * Delete a user
-     *
-     * @param id users id
-     */
-    void deleteUser(final Integer id);
+  /**
+   * Update a user
+   *
+   * @param id   users id
+   * @param user user data to be changed
+   * @return Updated user
+   */
+  User updateUser(final User user);
+
+  /**
+   * Delete a user
+   *
+   * @param id users id
+   */
+  void deleteUser(final User user);
 }
