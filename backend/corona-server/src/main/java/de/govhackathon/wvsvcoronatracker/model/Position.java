bbfe75c46b7f428ea0424f5225ed44ea1c88d10a
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-03-21 08:17:56 +0100

    feat(JPA): Basic model

---
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/Position.java
@@ -0,0 +1,32 @@
+package de.govhackathon.wvsvcoronatracker.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+public class Position {
+
+    private Integer id;
+
+    private Integer deviceId;
+
+    private String protocol;
+
+    private Boolean outdated;
+
+    private Boolean valid;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude ;
+
+    private BigDecimal altitude;
+
+    private Object network;
+}
