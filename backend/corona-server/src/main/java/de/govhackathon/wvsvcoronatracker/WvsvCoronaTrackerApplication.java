31961568a5911dccaf17d3e301fde8039b9b729a
Martin Esser <martin.esser@codeworx.digital>
2020-03-21 12:18:47 +0100

    Small fix to get application started again

--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/WvsvCoronaTrackerApplication.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/WvsvCoronaTrackerApplication.java
@@ -10,4 +10,5 @@ public class WvsvCoronaTrackerApplication {
 		SpringApplication.run(WvsvCoronaTrackerApplication.class, args);
 	}
 
+
 }
--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/PositionMapper.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/model/PositionMapper.java
@@ -1,7 +1,11 @@
 package de.govhackathon.wvsvcoronatracker.model;
 
 import de.ghwct.service.model.PositionDto;
-import org.mapstruct.*;
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+
 
 @Mapper(componentModel = "spring",
         collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
