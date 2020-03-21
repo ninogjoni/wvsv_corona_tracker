482bae41a76026a44d766ce67ced75f5c0cbfbdd
Thomas Heller <thomas.m.heller@gmail.com>
2020-03-21 13:48:14 +0100

    draft for body temp reminder cron

---
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/impl/BodyTempReminderServiceImpl.java
@@ -0,0 +1,48 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Send body temperature measurement reminder to users if
+ * they didn't already report their temperature recently.
+ */
+@Component
+public class BodyTempReminderServiceImpl {
+
+    private UsersService usersService;
+    private BodyTempService bodyTempService;
+    private MessagingService messagingService;
+
+    @Autowired
+    public JobServiceImpl(UsersService usersService, BodyTempService bodyTempService, MessagingService messagingService) {
+	this.usersService = usersService;
+	this.bodyTempService = bodyTempService;
+	this.messagingService = messagingService;
+    }
+
+    // Run at 08:00 daily
+    @Scheduled(cron = "0 0 8 * * *")
+    public void morningReminder() {
+	LocalDateTime earlyMorning = LocalDate.now().atTime(4, 0);
+
+	for(User user : this.usersService.getUsers()) {
+	    LocalDateTime last = this.bodyTempService.getLastBodyTempByUser(user.getId());
+	    if(last.isBefore(earlyMorning)) {
+		this.messagingService.sendMessage(user.getDeviceId(), "reminder"); // TODO: text
+	    }
+	}
+    }
+
+    // Run at 20:00 daily
+    @Scheduled(cron = "0 0 20 * * *")
+    public void eveningReminder() {
+	LocalDateTime earlyEvening = LocalDate.now().atTime(16, 0);
+
+	for(User user : this.usersService.getUsers()) {
+	    LocalDateTime last = this.bodyTempService.getLastBodyTempByUser(user.getId());
+	    if(last.isBefore(earlyMorning)) {
+		this.messagingService.sendMessage(user.getDeviceId(), "reminder"); // TODO: text
+	    }
+	}
+    }
+}
