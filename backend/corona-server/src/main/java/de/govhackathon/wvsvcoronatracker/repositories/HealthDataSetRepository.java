cbdd481be1ad4d66a7dc1f141d3e05b3c2f7318a
Thomas Eberle <Thomas@tommyelroy.com>
2020-03-22 10:42:54 +0100

    Added Method to find HealthDataSets by user and CreationDate Between.

--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/repositories/HealthDataSetRepository.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/repositories/HealthDataSetRepository.java
@@ -2,6 +2,7 @@ package de.govhackathon.wvsvcoronatracker.repositories;
 
 import de.govhackathon.wvsvcoronatracker.model.HealthDataSet;
 import de.govhackathon.wvsvcoronatracker.model.MedicalState;
+import de.govhackathon.wvsvcoronatracker.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.OffsetDateTime;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface HealthDataSetRepository extends CrudRepository<HealthDataSet, String> {
 
     List<HealthDataSet> findByMedicalStateAndCreationDateBetween(MedicalState medicalState, OffsetDateTime before, OffsetDateTime after);
+
+    List<HealthDataSet> findByUserAndMedicalStateAndCreationDateBetween(User user,MedicalState state, OffsetDateTime before, OffsetDateTime after);
 }
