c1037821066d859e2c58a01aadb767704e1b48d4
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-03-22 10:44:19 +0100

    fix(API): Correct password encoder error

--- backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/impl/ApiUserService.java
+++ backend/corona-server/src/main/java/de/govhackathon/wvsvcoronatracker/services/impl/ApiUserService.java
@@ -4,16 +4,13 @@ import de.govhackathon.wvsvcoronatracker.model.Role;
 import de.govhackathon.wvsvcoronatracker.model.system.AppConfig;
 import de.govhackathon.wvsvcoronatracker.repositories.AppConfigRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,49 +20,19 @@ public class ApiUserService implements UserDetailsService {
     private AppConfigRepository configRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         Optional<AppConfig> apiKey = this.configRepository.findById(AppConfig.API_KEY);
         Optional<AppConfig> apiSecret = this.configRepository.findById(AppConfig.API_SECRET);
-        if (apiKey.isPresent()) {
-            return new UserDetails() {
-                @Override
-                public Collection<? extends GrantedAuthority> getAuthorities() {
-                    List<GrantedAuthority> authorities
-                            = new ArrayList<>();
-                    authorities.add(new SimpleGrantedAuthority(Role.APP_USER.toString()));
-                    return authorities;
-                }
-
-                @Override
-                public String getPassword() {
-                    return apiSecret.get().getValue();
-                }
-
-                @Override
-                public String getUsername() {
-                    return apiKey.get().getValue();
-                }
-
-                @Override
-                public boolean isAccountNonExpired() {
-                    return true;
-                }
-
-                @Override
-                public boolean isAccountNonLocked() {
-                    return true;
-                }
-
-                @Override
-                public boolean isCredentialsNonExpired() {
-                    return true;
-                }
-
-                @Override
-                public boolean isEnabled() {
-                    return true;
-                }
-            };
+        if (apiKey.isPresent() && login.equalsIgnoreCase(apiKey.get().getValue())) {
+            User.UserBuilder users = User.withDefaultPasswordEncoder();
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+            UserDetails user = users
+                    .username(apiKey.get().getValue())
+                    .password(apiSecret.get().getValue())
+                    .roles(Role.APP_USER.name())
+                    .build();
+            manager.createUser(user);
+            return user;
         } else {
             return null;
         }
