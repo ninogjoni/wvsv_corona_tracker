be0acb3779850c8a0e9e60f417f8e8bfa938dc98
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-03-31 07:10:06 +0200

    fix(Android): Corrected Android signing issues

--- frontend/corona_tracker/android/fastlane/increment_version_code.sh
+++ frontend/corona_tracker/android/fastlane/increment_version_code.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-currentBuildNumber=$(cat local.properties | grep "flutter.versionCode" | cut -d "=" -f 2)
+currentBuildNumber=$(cat ../local.properties | grep "flutter.versionCode" | cut -d "=" -f 2)
 newBuildNumber=$(expr $currentBuildNumber + 1)
-awk '!/versionCode/' local.properties > temp && mv temp local.properties
-echo "flutter.versionCode=$newBuildNumber" >>local.properties
+awk '!/versionCode/' ../local.properties > temp && mv temp ../local.properties
+echo "flutter.versionCode=$newBuildNumber" >>../local.properties
