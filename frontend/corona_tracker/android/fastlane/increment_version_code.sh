8bba97c56aff13898fe4845d5a565f11ab7f76a9
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-03-31 07:34:55 +0200

    fix(Build): Corrected build numbering

--- frontend/corona_tracker/android/fastlane/increment_version_code.sh
+++ frontend/corona_tracker/android/fastlane/increment_version_code.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-currentBuildNumber=$(cat ../local.properties | grep "flutter.versionCode" | cut -d "=" -f 2)
+cd ..
+currentBuildNumber=$(cat local.properties | grep "flutter.versionCode" | cut -d "=" -f 2)
 newBuildNumber=$(expr $currentBuildNumber + 1)
-awk '!/versionCode/' ../local.properties > temp && mv temp ../local.properties
-echo "flutter.versionCode=$newBuildNumber" >>../local.properties
+awk '!/versionCode/' local.properties > temp && mv temp local.properties
+echo "flutter.versionCode=$newBuildNumber" >>local.properties
+cd ..
+build appbundle --target-platform android-arm,android-arm64,android-x64
